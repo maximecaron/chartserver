6ac395a4a3ce15505fd7f5896e23f9fee421a8e7
maximecaron <maxime.caron@gmail.com>
2014-03-30 17:11:09 -0700

    adding route to print sql version

--- src/github.com/maximecaron/chartserver/main/main.go
+++ src/github.com/maximecaron/chartserver/main/main.go
@@ -1,10 +1,29 @@
 package main
 
 import (
+        _ "github.com/lib/pq"
+        "database/sql"
         "github.com/ant0ine/go-json-rest"
         "net/http"
-        "github.com/lib/pq"
+        "log"
 )
+
+func GetVersion(w *rest.ResponseWriter, req *rest.Request) {
+        // Open does not directly open a database connection: this is deferred until a query is made
+        db, err := sql.Open("postgres", "sslmode=disable")
+        defer db.Close()
+        if err != nil {
+            log.Fatal(err)
+        }
+    
+        row := db.QueryRow("SELECT version();")
+        var value string
+        err2 := row.Scan(&value)
+        if err2 != nil {
+            log.Fatal(err)
+        }
+        w.WriteJson(&value)
+}
 
 type User struct {
         Id   string
@@ -23,6 +42,7 @@ func main() {
         handler := rest.ResourceHandler{}
         handler.SetRoutes(
                 rest.Route{"GET", "/users/:id", GetUser},
+                rest.Route{"GET", "/version/", GetVersion},
         )
         http.ListenAndServe(":8080", &handler)
 }
